3e06e47f87bb8b7f69902bad48c1456adf5c5113
Jamie Quigley <jamie@quigley.xyz>
2022-12-01 21:24:19 +0000

    Improve documentation and readme

--- readme.md
+++ readme.md
@@ -6,6 +6,17 @@ The AoC struct is the main entry point for this library.
 
 See `AoC::new` and `AoC::read_or_fetch` for usage
 
+# Example
+
+```rs
+fn main() -> Result<(), Box<dyn Error>> {
+    let aoc = AoC::new(2020)?; // year 2020
+    let input = aoc.read_or_fetch(1)?; // day 01
+    solve(&input); // Implementation of `solve` left as an exercise to the reader :)
+    Ok(())
+}
+```
+
 ---
 
 Available under the terms of version 2.0 of the Mozilla Public Licence
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! Emergenv
+//! Emergence is a library to fetch and cache Advent of Code inputs.
+//! 
+//! The [`AoC`] struct is the main entry point for this library.
+//! 
+//! See [`AoC::new`] and [`AoC::read_or_fetch`] for usage
+//!
+//! # Example 
+//!
+//! ```
+//! # use emergence::AoC;
+//! # use std::error::Error;
+//! # fn solve(input: &str) {}
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let aoc = AoC::new(2020)?; // year 2020
+//!     let input = aoc.read_or_fetch(1)?; // day 01
+//!     solve(&input); // Implementation of `solve` left as an exercise to the reader :)
+//!     Ok(())
+//! }
+//! ```
 use std::{
     io,
     path::{Path, PathBuf},
